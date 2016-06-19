4bca22f1694580f6babf680dbcc9bb9eeb1ccd24
Jack Wang <jj22wang@uwaterloo.ca>
2016-06-19 11:26:46 -0400

    Big Player Class changes, Sadman please read

--- Player.cpp
+++ Player.cpp
@@ -2,24 +2,52 @@
 #include <iostream>
 #include <cassert>
 
+bool checkLegality(const Card& first, const Card& second){
+    if(first.getRank() == second.getRank()){
+        return true;
+    }
+
+    if(first.getRank()+ 1 == second.getRank() || first.getRank() - 1 == second.getRank()){
+        if(first.getSuit() == second.getSuit()){
+            return true;
+        }
+    }
+}
 Player::Player(){}
 
 void Player::addCard(const Card* card) {
     hand_.insert(card);
 }
 
-void Player::playCard(const Card* card){
-    hand_.erase(card);
+void Player::playCard(const Card* card, std::vector<const Card*> played_cards){
+    std::vector<const Card*> legalPlays = legalMoves(played_cards);
+    //assert(std::find(legalPlays.begin(), legalPlays.end(), card) != legalMoves.end()); TODO, I have no idea why this pass failed
 
-    std::cerr << "Playing Card Not In Hand" << std::endl;       // Fail on playing card not in hand, can also check for legality
+    hand_.erase(card);
+    played_cards.push_back(card);
 }
 
-void Player::discardCard(const Card* card){
-    playCard(card);                                             // if play changes to check for legality, then must change implementation
+void Player::discardCard(const Card* card, const std::vector<const Card*> played_cards){
+    assert(legalMoves(played_cards).size() == 0);
+    hand_.erase(card);
     discard_.push_back(card);                                   // Same as play except add to discard pile
 }
 
-const Card** Player::legalMoves(const Card* card){
+std::vector<const Card*> Player::legalMoves(const std::vector<const Card*> played_cards){
+    std::vector<const Card*> legalPlays;
+
+    std::set<const Card*>::iterator card;
+    for(card = hand_.begin(); card != hand_.end(); ++card){
+        for(int j = 0; j < played_cards.size(); j++){
+            if(checkLegality(**card, *played_cards[j]))
+            {
+                legalPlays.push_back(*card);
+                break;
+            }
+        }
+    }
+
+    return legalPlays;
 }
 
 void Player::updateScore(){
@@ -35,4 +63,3 @@ void Player::endRound(){
 int Player::getScore(){
     return score_;
 }
-
--- Player.h
+++ Player.h
@@ -10,9 +10,9 @@ class Player {
 public:
     Player();                                       // Default Constructor
     void addCard(const Card*);                      // adds a card to the hand
-    void playCard(const Card*);                     // play a legal card
-    void discardCard(const Card*);                  // discard a card if no plays can be made
-    const Card** legalMoves(const Card*);            // return a set of legal moves
+    void playCard(const Card*, std::vector<const Card*> played_cards);                     // play a legal card
+    void discardCard(const Card*, const std::vector<const Card*> );                  // discard a card if no plays can be made
+    std::vector<const Card*>  legalMoves(const std::vector<const Card*> );            // return a set of legal moves
     void endRound();                                // ends the round
     int getScore();                                // gets current score of player
 protected:
